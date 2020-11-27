d0818c860587c59e9c5c713b49c9ec1d4fe46a17
yoonseok <yoonseok@youcanstar.com>
2020-11-27 11:15:11 +0900

    Updates

---
+++ precache-manifest.ebf1135f0c4e4b86ae637d1619565eae.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "9098b63d50b813139666",
+    "url": "/static/js/app.1cfbf9df.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "edee2c0dc6c8fca8c735",
+    "url": "/static/js/2.7dc474f3.chunk.js"
+  },
+  {
+    "revision": "004c2bbb035d8d06bb830efc4673c886",
+    "url": "/static/media/star.69c71386.png"
+  },
+  {
+    "revision": "2327736b3ea09c41abfb69be1221f430",
+    "url": "/static/media/heart.23b5c2c0.png"
+  },
+  {
+    "revision": "51671417ef20e0bbc32f0a2bc6edfa95",
+    "url": "/static/media/rocket.d2f3bf41.png"
+  },
+  {
+    "revision": "a7b9dc9de5f8f1fb1afbef917619a5ac",
+    "url": "/static/media/bell.cf04af27.png"
+  },
+  {
+    "revision": "722e5f70ab8695fb2ed47602de3aa533",
+    "url": "/static/media/login.6d9f9238.png"
+  },
+  {
+    "revision": "dbd6cee49a472cc285b520320cd87bca",
+    "url": "/static/media/register.034666df.png"
+  },
+  {
+    "revision": "0d45c5b56c28152307fa6d58c5fd6205",
+    "url": "/static/media/forget.b210f38c.png"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "277289c53af7cb469c1dc5dca3adca35",
+    "url": "/./fonts/Ubuntu_300Light.ttf"
+  },
+  {
+    "revision": "d96027148c57a715e372789a90f69e8f",
+    "url": "/./fonts/Ubuntu_300Light_Italic.ttf"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "/./fonts/Ubuntu_400Regular.ttf"
+  },
+  {
+    "revision": "4b96047e4af086277cdaeb9e60857534",
+    "url": "/./fonts/Ubuntu_400Regular_Italic.ttf"
+  },
+  {
+    "revision": "8e22c2a6e3a3c679787e763a97fa11f7",
+    "url": "/./fonts/Ubuntu_500Medium.ttf"
+  },
+  {
+    "revision": "0fbe070c207e3a869cc9e02f234f81e5",
+    "url": "/./fonts/Ubuntu_500Medium_Italic.ttf"
+  },
+  {
+    "revision": "e00e2a77dd88a8fe75573a5d993af76a",
+    "url": "/./fonts/Ubuntu_700Bold.ttf"
+  },
+  {
+    "revision": "48c161df9991f9b0f6e4a858e95e415e",
+    "url": "/./fonts/Ubuntu_700Bold_Italic.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "4fb6d7b5e45c58295083cbbbe039ae5f",
+    "url": "/static/media/icon.ef89f69f.png"
+  },
+  {
+    "revision": "606b444c1d0ab2616a11663519d05791",
+    "url": "/static/media/splash.894e7fd6.png"
+  },
+  {
+    "revision": "8a020cddcb5ed44f8d670d3fb718071a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "e802d2644326fd9fa701f1d969a8a94a",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "fe9a6d0e9fb99bf956ba337139fda227",
+    "url": "/static/js/2.7dc474f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d324ddc823eca6c86688f62576c81e76",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "e72daee58e37c99d3553059eda418aa8",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "50b1bd72d8b0c569ffcad83d622ec176",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "f8687363632968a5c8b679379b5124bc",
+    "url": "/manifest.json"
+  }
+];
